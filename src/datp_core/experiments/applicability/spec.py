1dba919eb642abcc7b4e85e988ffc46efb621acb
Salaheddine Naslouby <naslouby.salahe@gmail.com>
2026-08-08 13:09:33 +0100

    Add CICIoT2023 applicability-boundary specification

---
+++ src/datp_core/experiments/applicability/spec.py
@@ -0,0 +1,26 @@
+"""CICIoT2023 file-defined pseudo-client applicability-boundary specification."""
+
+from datp_core.analysis.metrics.contracts import OPERATING_POINT_METRICS
+from datp_core.core.identifiers import (
+    EvidenceRole,
+    ExperimentId,
+    ExperimentReadiness,
+    FederatedThresholdMethod,
+    PopulationId,
+    PreprocessingProtocolId,
+    TrainingModelId,
+)
+from datp_core.experiments.common.coordinates import ExperimentSpec
+
+CICIOT_FILE_CLIENT_BOUNDARY = ExperimentSpec(
+    id=ExperimentId.CICIOT_FILE_CLIENT_BOUNDARY,
+    role=EvidenceRole.APPLICABILITY_BOUNDARY,
+    population=PopulationId.CICIOT_FILE_CLIENTS,
+    training_model=TrainingModelId.FEDAVG_AUTOENCODER,
+    preprocessing_protocol=PreprocessingProtocolId.FEDERATED_CLIENT_LOCAL_STANDARD,
+    federated_thresholds=(FederatedThresholdMethod.SHARED_THRESHOLD, FederatedThresholdMethod.LOCAL_THRESHOLD),
+    metrics=OPERATING_POINT_METRICS,
+    readiness=ExperimentReadiness.DECLARED,
+)
+
+APPLICABILITY_EXPERIMENTS = (CICIOT_FILE_CLIENT_BOUNDARY,)
